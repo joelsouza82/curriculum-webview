fb84dd3341239e34419d155ce151ddc8497463c8
Joel de Almeida Souza <joelsouza82@gmail.com>
2026-07-31 18:29:28 -0300

    Inclui botao de voltar

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -5,22 +5,8 @@ import { getPersonals } from '../../services/personalService';
 import styles from './page.module.css'; // Estilos específicos da página de busca
 import { useRouter } from 'next/navigation';
 
-interface PersonalData {
-  id_personal: string;
-  address: string;
-  neighborhood: string;
-  city: string;
-  state: string;
-  cep: string;
-  phone: string;
-  email: string;
-  website: string;
-  linkedin: string;
-  github: string;
-}
-
 export default function SearchPage() {
-  const [personals, setPersonals] = useState<PersonalData[]>([]);
+  const [personals, setPersonals] = useState<Personal[]>([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<string | null>(null);
 
@@ -47,6 +33,18 @@ export default function SearchPage() {
 
   return (
     <main className={styles.main}>
+      <div className="w-full mb-4">
+        <button
+          className={`${styles.backButton} flex items-center`}
+          onClick={handleBackToHome}
+        >
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5 mr-2">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18" />
+          </svg>
+          Voltar para Home
+        </button>
+      </div>
+
       <div className={styles.titleContainer}>
         <h1 className={styles.title}>Currículos Encontrados</h1>
       </div>
@@ -77,15 +75,6 @@ export default function SearchPage() {
         </div>
       )}
 
-      <button
-        className={`${styles.backButton} mt-8`}
-        onClick={handleBackToHome}
-      >
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
-          <path strokeLinecap="round" strokeLinejoin="round" d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18" />
-        </svg>
-        Voltar para Home
-      </button>
     </main>
   );
 }
