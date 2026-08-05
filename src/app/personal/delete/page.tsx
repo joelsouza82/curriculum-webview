a865484de87a67fe85f39850eba8280d84eaec51
Joel de Almeida Souza <joelsouza82@gmail.com>
2026-08-05 20:03:29 -0300

    Implementa delete parte 2

--- src/app/personal/delete/page.tsx
+++ src/app/personal/delete/page.tsx
@@ -34,11 +34,18 @@ const inboxPath = (
 );
 
 const trashPath = (
-  <path
-    strokeLinecap="round"
-    strokeLinejoin="round"
-    d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
-  />
+  <>
+    <path strokeLinecap="round" strokeLinejoin="round" d="M9.75 3.104a2.25 2.25 0 0 1 4.5 0V4.5h-4.5V3.104Z" />
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M4.5 4.5h15M6 4.5v13.5A2.25 2.25 0 0 0 8.25 20.25h7.5A2.25 2.25 0 0 0 18 18V4.5M9.75 9.75v6m4.5-6v6"
+    />
+  </>
+);
+
+const xMarkPath = (
+  <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
 );
 
 function DeleteForm() {
@@ -162,6 +169,7 @@ function DeleteForm() {
                 onClick={() => goToSearch(loginId)}
                 disabled={deleting}
               >
+                <Icon className={styles.buttonIcon}>{xMarkPath}</Icon>
                 Cancelar
               </button>
               <button
--- src/app/personal/update/page.tsx
+++ src/app/personal/update/page.tsx
@@ -161,12 +161,7 @@ function UpdateForm() {
                 <path
                   strokeLinecap="round"
                   strokeLinejoin="round"
-                  d="M19 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h11l5 5v11a2 2 0 0 1-2 2z"
-                />
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M17 21v-8H7v8M7 3v5h8"
+                  d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10"
                 />
               </svg>
               {loading ? 'Atualizando...' : 'Salvar Alterações'}
