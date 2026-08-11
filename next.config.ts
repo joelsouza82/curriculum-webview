7dc7566e3a94f83bf9da59be00a010146e54ab97
Joel de Almeida Souza <joelsouza82@gmail.com>
2026-08-10 23:29:16 -0300

    Troca de dominio de API

--- next.config.ts
+++ next.config.ts
@@ -6,7 +6,7 @@ const nextConfig: NextConfig = {
     return [
       {
         source: '/api/:path*', // Qualquer requisição para /api/ será redirecionada
-        destination: 'https://go-api-nzg1.onrender.com/:path*', // Para a sua API
+        destination: 'https://curriculum-go.onrender.com/:path*', // Para a sua API
       },
     ];
   },
