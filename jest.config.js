393e9ef8b042bb2dae92486d40d941881bbc9cfc
Joel de Almeida Souza <joelsouza82@gmail.com>
2026-08-23 13:05:13 -0300

    Implementa pipes do github

--- jest.config.js
+++ jest.config.js
@@ -11,6 +11,14 @@ const config = {
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/.next/'],
+  coverageThreshold: {
+    global: {
+      branches: 90,
+      functions: 90,
+      lines: 90,
+      statements: 90,
+    },
+  },
 };
 
 module.exports = createJestConfig(config);
